6dfa5dedcc4a5caebb868d1d27b447cf3270eab0
Talal Ait-Mouh <148981557+Talal-Aitmouh@users.noreply.github.com>
2024-07-15 21:43:48 +0100

    MINOR changes in booking treatment

--- admin/booking.php
+++ admin/booking.php
@@ -43,23 +43,63 @@ include './traitement/booking.php'
                         </div>
                         <div class="card-body">
                             <!-- Modal -->
+
+
+                            <div class="table-responsive">
+                                <table id="add-row" class="display table table-striped table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>Name Room</th>
+                                            <th>Guest</th>
+                                            <th>Date</th>
+                                            <th>Status</th>
+                                            <th>Total Amount</th>
+                                            <th style="width: 10%">Action</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <?php foreach ($reservations as $reservation) : ?>
+                                            <tr>
+                                                <td><?php echo $reservation['RoomName']; ?></td>
+                                                <td><?php echo $reservation['GuestName']; ?></td>
+                                                <td><?php echo $reservation['CheckInDate']; ?> --> <?php echo $reservation['CheckOutDate']; ?></td>
+                                                <td><?php echo $reservation['Status']; ?></td>
+                                                <td><?php echo $reservation['TotalAmount']; ?></td>
+                                                <td>
+                                                    <form action="./traitement/booking.php" method="POST">
+                                                        <div class="form-button-action">
+                                                            <input type="hidden" name="bookingID" value="<?php echo $reservation['BookingID']; ?>">
+                                                            <button class="btn btn-link btn-primary btn-lg" type="button" data-bs-toggle="modal" name="edit" data-bs-target="#edit">
+                                                                <i class="fa fa-edit"></i>
+                                                            </button>
+                                                            <button type="submit" data-bs-toggle="tooltip" title="" class="btn btn-link btn-danger" name="delete" data-original-title="Remove">
+                                                                <i class="fa fa-times"></i>
+                                                            </button>
+                                                        </div>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach; ?>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- Modal -->
                             <div class="modal fade" id="addRowModal" tabindex="-1" role="dialog" aria-hidden="true">
                                 <div class="modal-dialog modal-xl" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header border-0">
                                             <h5 class="modal-title">
                                                 <span class="fw-mediumbold"> New</span>
-                                                <span class="fw-light"> Row </span>
+                                                <span class="fw-light"> Reservation </span>
                                             </h5>
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                 <span aria-hidden="true">&times;</span>
                                             </button>
                                         </div>
                                         <div class="modal-body">
-                                            <p class="small">
-                                                Create a new row using this form, make sure you fill
-                                                them all
-                                            </p>
+
                                             <form action="./traitement/booking.php" method="POST">
                                                 <h4>Guest Info</h4>
                                                 <div class="row">
@@ -154,46 +194,114 @@ include './traitement/booking.php'
                                     </div>
                                 </div>
                             </div>
+                            <!-- end -->
+                            <!--Edit Modal  -->
+                            <?php foreach ($reservations as $reservation) : ?>
+                                <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-hidden="true">
+                                    <div class="modal-dialog modal-xl" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header border-0">
+                                                <h5 class="modal-title">
+                                                    <span class="fw-mediumbold"> Update</span>
+                                                    <span class="fw-light"> Reservation </span>
+                                                </h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
 
-                            <div class="table-responsive">
-                                <table id="add-row" class="display table table-striped table-hover">
-                                    <thead>
-                                        <tr>
-                                            <th>Name Room</th>
-                                            <th>Guest</th>
-                                            <th>Date</th>
-                                            <th>Status</th>
-                                            <th>Total Amount</th>
-                                            <th style="width: 10%">Action</th>
-                                        </tr>
-                                    </thead>
+                                                <form action="./traitement/booking.php" method="POST">
 
-                                    <tbody>
-                                        <?php foreach ($reservations as $reservation) : ?>
-                                            <tr>
-                                                <td><?php echo $reservation['RoomName']; ?></td>
-                                                <td><?php echo $reservation['GuestName']; ?></td>
-                                                <td><?php echo $reservation['CheckInDate']; ?> --> <?php echo $reservation['CheckOutDate']; ?></td>
-                                                <td><?php echo $reservation['Status']; ?></td>
-                                                <td><?php echo $reservation['TotalAmount']; ?></td>
-                                                <td>
-                                                    <form action="">
-                                                        <div class="form-button-action">
-                                                            <button type="button" data-bs-toggle="tooltip" title="" class="btn btn-link btn-primary btn-lg" data-original-title="Edit Task">
-                                                                <i class="fa fa-edit"></i>
-                                                            </button>
-                                                            <button type="button" data-bs-toggle="tooltip" title="" class="btn btn-link btn-danger" data-original-title="Remove">
-                                                                <i class="fa fa-times"></i>
-                                                            </button>
+                                                    <div class="row">
+                                                        <div class="col-md-4">
+                                                            <div class="form-group form-group-default">
+                                                                <label>Name Guest</label>
+                                                                <input name="name" value="<?php echo $reservation['GuestName']; ?>" id="addName" type="text" class="form-control" placeholder="Enter name" />
+                                                            </div>
                                                         </div>
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                        <?php endforeach; ?>
+                                                        <div class="col-md-4">
+                                                            <div class="form-group form-group-default">
+                                                                <label>Room</label>
+                                                                <input name="phone" id="addPhone" type="text" value="<?php echo $reservation['RoomName']; ?>" class="form-control" placeholder="Enter phone number" />
 
-                                    </tbody>
-                                </table>
-                            </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-4">
+                                                            <div class="form-group form-group-default">
+                                                                <label>Total Amount</label>
+                                                                <input name="nationality" value="<?php echo $reservation['TotalAmount']; ?>" id="addNationality" type="text" class="form-control" placeholder="Enter nationality" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-4">
+                                                            <div class="form-group form-group-default">
+                                                                <label>Status</label>
+                                                                <input name="email" value="<?php echo $reservation['Status']; ?>" id="addEmail" type="email" class="form-control" placeholder="Enter email" />
+                                                            </div>
+                                                        </div>
+
+
+
+
+                                                        <div class="col-md-4">
+                                                            <div class="form-group form-group-default">
+                                                                <label>Check-In Date</label>
+                                                                <input name="checkInDate" value="<?php echo $reservation['CheckInDate']; ?>" id="addCheckInDate" type="date" class="form-control" placeholder="Select check-in date" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-4">
+                                                            <div class="form-group form-group-default">
+                                                                <label>Check-Out Date</label>
+                                                                <input name="checkOutDate" value="<?php echo $reservation['CheckOutDate']; ?>" id="addCheckOutDate" type="date" class="form-control" placeholder="Select check-out date" />
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    <h4>Select Rooms</h4>
+                                                    <div class="row">
+                                                        <div class="col-sm-6">
+                                                            <div class="form-group form-group-default">
+                                                                <label>Rooms</label>
+                                                                <select name="roomID" class="form-select" id="exampleFormControlSelect1" aria-label="Default select example">
+                                                                    <option selected disabled>Open this select menu</option>
+                                                                    <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+                                                                        <option value="<?php echo $row['RoomID']; ?>"><?php echo $row['RoomName']; ?></option>
+                                                                    <?php endwhile; ?>
+                                                                </select>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-sm-6">
+                                                            <div class="form-group form-group-default">
+                                                                <label>Quantity</label>
+                                                                <input name="quantity" class="form-control" type="number" id="html5-number-input" min="1" />
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    <h4>Add service</h4>
+                                                    <div class="row">
+                                                        <div class="col-sm-12">
+                                                            <div class="form-group form-group-default">
+                                                                <label>Service</label>
+                                                                <select name="services[]" class="form-select" multiple id="exampleFormControlSelect2" aria-label="Default select example">
+                                                                    <?php while ($row = mysqli_fetch_assoc($resultServices)) : ?>
+                                                                        <option value="<?php echo $row['ServiceID']; ?>"><?php echo $row['Name']; ?></option>
+                                                                    <?php endwhile; ?>
+                                                                </select>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="modal-footer border-0">
+                                                        <button type="submit" class="btn btn-primary">Update</button>
+                                                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php endforeach; ?>
                         </div>
                     </div>
                 </div>
--- admin/traitement/booking.php
+++ admin/traitement/booking.php
@@ -10,8 +10,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
-$reservations =[];
+$reservations = [];
 $sqlbooking = "SELECT 
     Booking.BookingID,
     Booking.CheckInDate,
@@ -25,52 +24,63 @@ FROM
 INNER JOIN 
     Guests ON Booking.GuestID = Guests.GuestID
 INNER JOIN 
-    Rooms ON Booking.RoomID = Rooms.RoomID;
-";
+    Rooms ON Booking.RoomID = Rooms.RoomID";
 $resultbooking = $conn->query($sqlbooking);
 
-if ($resultbooking->num_rows > 0){
-    while($rowbooking = $resultbooking->fetch_assoc()) {
+if ($resultbooking->num_rows > 0) {
+    while ($rowbooking = $resultbooking->fetch_assoc()) {
         $reservations[] = $rowbooking;
     }
 }
 
-$queryroom = "SELECT RoomID, RoomName, Price, quantity FROM Rooms";
+$queryroom = "SELECT RoomID, RoomName, Price, Quantity FROM Rooms";
 $result = mysqli_query($conn, $queryroom);
 
 $queryServices = "SELECT ServiceID, Name, Amount FROM Service";
 $resultServices = mysqli_query($conn, $queryServices);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Collect guest information
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $address = $_POST['address'];
-    $nationality = $_POST['nationality'];
-    $phone = $_POST['phone'];
-    $checkInDate = $_POST['checkInDate'];
-    $checkOutDate = $_POST['checkOutDate'];
+    if (isset($_POST['delete'])) {
+        $bookingID = $_POST['bookingID'];
 
-    // Insert guest information
-    $query = "INSERT INTO Guests (Name, Email, Address, Nationality, Phone, CheckInDate, CheckOutDate) VALUES ('$name', '$email', '$address', '$nationality', '$phone', '$checkInDate', '$checkOutDate')";
-    if (mysqli_query($conn, $query)) {
-        $guestID = mysqli_insert_id($conn);
-
-        // Collect booking information
-        $roomID = $_POST['roomID'];
-        $quantity = $_POST['quantity'];
-
-        // Fetch room price and quantity
-        $query = "SELECT Price, quantity FROM Rooms WHERE RoomID = '$roomID'";
+        // Handle delete
+        $query = "SELECT RoomID, CheckInDate, CheckOutDate FROM Booking WHERE BookingID = '$bookingID'";
         $result = mysqli_query($conn, $query);
-        $room = mysqli_fetch_assoc($result);
-        $roomPrice = $room['Price'];
-        $roomQuantity = $room['quantity'];
+        if ($result && mysqli_num_rows($result) > 0) {
+            $booking = mysqli_fetch_assoc($result);
+            $roomID = $booking['RoomID'];
 
-        // Check if enough rooms are available
-        if ($roomQuantity < $quantity) {
-            die("Not enough rooms available.");
+            // Calculate number of days booked
+            $checkInDate = new DateTime($booking['CheckInDate']);
+            $checkOutDate = new DateTime($booking['CheckOutDate']);
+            $interval = $checkInDate->diff($checkOutDate);
+            $numDays = $interval->days;
+
+            // Get the quantity of rooms booked
+            $quantity = 1; // Assuming quantity is 1, if not adjust this based on your requirement
+
+            // Increase the room quantity
+            $query = "UPDATE Rooms SET Quantity = Quantity + $quantity WHERE RoomID = '$roomID'";
+            mysqli_query($conn, $query);
+
+            // Delete the booking
+            $query = "DELETE FROM Booking WHERE BookingID = '$bookingID'";
+            mysqli_query($conn, $query);
         }
+        header('Location: ../booking.php');
+        exit;
+
+    } elseif (isset($_POST['update'])) {
+        $bookingID = $_POST['bookingID'];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $address = $_POST['address'];
+        $nationality = $_POST['nationality'];
+        $phone = $_POST['phone'];
+        $checkInDate = $_POST['checkInDate'];
+        $checkOutDate = $_POST['checkOutDate'];
+        $roomID = $_POST['roomID'];
+        $quantity = 1; // Assuming quantity is 1, if not adjust this based on your requirement
 
         // Calculate the number of days booked
         $date1 = new DateTime($checkInDate);
@@ -78,32 +88,110 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $interval = $date1->diff($date2);
         $numDays = $interval->days;
 
+        // Get room price
+        $query = "SELECT Price FROM Rooms WHERE RoomID = '$roomID'";
+        $result = mysqli_query($conn, $query);
+        $room = mysqli_fetch_assoc($result);
+        $roomPrice = $room['Price'];
+
         // Calculate total amount for rooms
         $totalAmount = $roomPrice * $quantity * $numDays;
 
-        // Collect and insert services if any
+        // Update booking
+        $query = "UPDATE Booking SET CheckInDate='$checkInDate', CheckOutDate='$checkOutDate', TotalAmount='$totalAmount', RoomID='$roomID' WHERE BookingID='$bookingID'";
+        mysqli_query($conn, $query);
+
+        // Update guest information
+        $query = "UPDATE Guests SET Name='$name', Email='$email', Address='$address', Nationality='$nationality', Phone='$phone' WHERE GuestID=(SELECT GuestID FROM Booking WHERE BookingID='$bookingID')";
+        mysqli_query($conn, $query);
+
+        // Update services
+        // First, delete existing service bookings
+        $query = "DELETE FROM ServiceBooking WHERE BookingID = '$bookingID'";
+        mysqli_query($conn, $query);
+
+        // Then, add new service bookings
         if (isset($_POST['services'])) {
             foreach ($_POST['services'] as $serviceID) {
+                $query = "INSERT INTO ServiceBooking (BookingID, ServiceID) VALUES ('$bookingID', '$serviceID')";
+                mysqli_query($conn, $query);
+
+                // Update the total amount with service cost
                 $query = "SELECT Amount FROM Service WHERE ServiceID = '$serviceID'";
                 $result = mysqli_query($conn, $query);
                 $service = mysqli_fetch_assoc($result);
                 $totalAmount += $service['Amount'];
+
+                // Update the total amount in the booking
+                $query = "UPDATE Booking SET TotalAmount='$totalAmount' WHERE BookingID='$bookingID'";
+                mysqli_query($conn, $query);
             }
         }
 
-        // Insert booking information
-        $status = 'Booked'; // Initial status
-        $query = "INSERT INTO Booking (CheckInDate, CheckOutDate, TotalAmount, Status, GuestID, RoomID) VALUES ('$checkInDate', '$checkOutDate', '$totalAmount', '$status', '$guestID', '$roomID')";
-        mysqli_query($conn, $query);
+        header('Location: ../booking.php');
+        exit;
 
-        // Optionally, link services to the booking if any
-       
+    } else {
+        // Handle new booking
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $address = $_POST['address'];
+        $nationality = $_POST['nationality'];
+        $phone = $_POST['phone'];
+        $checkInDate = $_POST['checkInDate'];
+        $checkOutDate = $_POST['checkOutDate'];
+        $roomID = $_POST['roomID'];
+        $quantity = 1; // Assuming quantity is 1, if not adjust this based on your requirement
 
-        // Decrement the quantity of rooms available
-        $newRoomQuantity = $roomQuantity - $quantity;
-        $query = "UPDATE Rooms SET quantity = '$newRoomQuantity' WHERE RoomID = '$roomID'";
-        mysqli_query($conn, $query);
+        // Insert guest information
+        $query = "INSERT INTO Guests (Name, Email, Address, Nationality, Phone) VALUES ('$name', '$email', '$address', '$nationality', '$phone')";
+        if (mysqli_query($conn, $query)) {
+            $guestID = mysqli_insert_id($conn);
+
+            // Fetch room price and quantity
+            $query = "SELECT Price, Quantity FROM Rooms WHERE RoomID = '$roomID'";
+            $result = mysqli_query($conn, $query);
+            $room = mysqli_fetch_assoc($result);
+            $roomPrice = $room['Price'];
+            $roomQuantity = $room['Quantity'];
+
+            // Check if enough rooms are available
+            if ($roomQuantity < $quantity) {
+                die("Not enough rooms available.");
+            }
+
+            // Calculate the number of days booked
+            $date1 = new DateTime($checkInDate);
+            $date2 = new DateTime($checkOutDate);
+            $interval = $date1->diff($date2);
+            $numDays = $interval->days;
+
+            // Calculate total amount for rooms
+            $totalAmount = $roomPrice * $quantity * $numDays;
+
+            // Collect and insert services if any
+            if (isset($_POST['services'])) {
+                foreach ($_POST['services'] as $serviceID) {
+                    $query = "SELECT Amount FROM Service WHERE ServiceID = '$serviceID'";
+                    $result = mysqli_query($conn, $query);
+                    $service = mysqli_fetch_assoc($result);
+                    $totalAmount += $service['Amount'];
+                }
+            }
+
+            // Insert booking information
+            $status = 'Booked'; // Initial status
+            $query = "INSERT INTO Booking (CheckInDate, CheckOutDate, TotalAmount, Status, GuestID, RoomID) VALUES ('$checkInDate', '$checkOutDate', '$totalAmount', '$status', '$guestID', '$roomID')";
+            mysqli_query($conn, $query);
+
+            // Decrement the quantity of rooms available
+            $newRoomQuantity = $roomQuantity - $quantity;
+            $query = "UPDATE Rooms SET Quantity = '$newRoomQuantity' WHERE RoomID = '$roomID'";
+            mysqli_query($conn, $query);
+
+            header('Location: ../booking.php');
+            exit;
+        }
     }
-    header('Location: ../booking.php');
 }
 ?>
