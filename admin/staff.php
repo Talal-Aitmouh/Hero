d0f20e7c0b94d6fe706a63ab0b61e72b8c63f29f
Talal Ait-Mouh <148981557+Talal-Aitmouh@users.noreply.github.com>
2024-07-17 00:33:33 +0100

    MINOR changes in treatment staff

--- admin/staff.php
+++ admin/staff.php
@@ -8,107 +8,119 @@ include './traitement/staff.php'
 
 <body>
     <div class="wrapper">
-    <?php
+        <?php
         $current_page = 'staff';
         include './links/sidebare.php'; ?>
     </div>
-        <!-- end sidebar -->
-        <div class="main-panel">
-            <?php include './links/customer.php' ?>
-            <div class="container">
-                <div class="page-inner">
-                    <div class="page-header">
-                        <h3 class="fw-bold mb-3">Staff</h3>
-                        <ul class="breadcrumbs mb-3">
-                            <li class="nav-home">
-                                <a href="#">
-                                    <i class="fas fas fa-users"></i>
-                                </a>
-                            </li>
+    <!-- end sidebar -->
+    <div class="main-panel">
+        <?php include './links/customer.php' ?>
+        <div class="container">
+            <div class="page-inner">
+                <div class="page-header">
+                    <h3 class="fw-bold mb-3">Staff</h3>
+                    <ul class="breadcrumbs mb-3">
+                        <li class="nav-home">
+                            <a href="#">
+                                <i class="fas fas fa-users"></i>
+                            </a>
+                        </li>
 
-                        </ul>
-                    </div>
+                    </ul>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <div class="d-flex align-items-center">
-                                    <h4 class="card-title">Add Staff</h4>
-                                    <button class="btn btn-primary btn-round ms-auto" data-bs-toggle="modal" data-bs-target="#addRowModal">
-                                        <i class="fa fa-plus"></i>
-                                        Add Staff
-                                    </button>
-                                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="d-flex align-items-center">
+                                <h4 class="card-title">Add Staff</h4>
+                                <button class="btn btn-primary btn-round ms-auto" data-bs-toggle="modal" data-bs-target="#addRowModal">
+                                    <i class="fa fa-plus"></i>
+                                    Add Staff
+                                </button>
                             </div>
-                            <div class="card-body">
-                                <!-- Modal -->
-                                <div class="modal fade" id="addRowModal" tabindex="-1" role="dialog" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header border-0">
-                                                <h5 class="modal-title">
-                                                    <span class="fw-mediumbold"> New</span>
-                                                    <span class="fw-light"> Row </span>
-                                                </h5>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <p class="small">
-                                                    Create a new row using this form, make sure you
-                                                    fill them all
-                                                </p>
-                                                <form>
-                                                    <div class="row">
-                                                        <div class="col-sm-12">
-                                                            <div class="form-group form-group-default">
-                                                                <label>Name</label>
-                                                                <input id="addName" type="text" class="form-control" placeholder="fill name" />
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-md-6 pe-0">
-                                                            <div class="form-group form-group-default">
-                                                                <label>Position</label>
-                                                                <input id="addPosition" type="text" class="form-control" placeholder="fill position" />
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-md-6">
-                                                            <div class="form-group form-group-default">
-                                                                <label>Office</label>
-                                                                <input id="addOffice" type="text" class="form-control" placeholder="fill office" />
-                                                            </div>
+                        </div>
+                        <div class="card-body">
+                            <!-- Modal -->
+                            <div class="modal fade" id="addRowModal" tabindex="-1" role="dialog" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header border-0">
+                                            <h5 class="modal-title">
+                                                <span class="fw-mediumbold"> New</span>
+                                                <span class="fw-light"> Staff </span>
+                                            </h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+
+                                            <form action="./traitement/staff.php" method="post">
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group form-group-default">
+                                                            <label>Full Name</label>
+                                                            <input id="addname" name="name" type="text" class="form-control" placeholder="fill name" />
                                                         </div>
                                                     </div>
-                                                </form>
-                                            </div>
-                                            <div class="modal-footer border-0">
-                                                <button type="button" id="addRowButton" class="btn btn-primary">
-                                                    Add
-                                                </button>
-                                                <button type="button" class="btn btn-danger" data-dismiss="modal">
-                                                    Close
-                                                </button>
-                                            </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group form-group-default">
+                                                            <label>Position</label>
+                                                            <input id="addPosition" name="position" type="text" class="form-control" placeholder="fill position" />
+                                                        </div>
+                                                    </div>
+
+
+                                                    <div class="col-sm-12">
+                                                        <div class="form-group form-group-default">
+                                                            <label>Email</label>
+                                                            <input id="addemail" name="email" type="email" class="form-control" placeholder="fill email" />
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group form-group-default">
+                                                            <label>Phone</label>
+                                                            <input id="addphone" name="phone" type="number" class="form-control" placeholder="fill phone" />
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group form-group-default">
+                                                            <label>Password</label>
+                                                            <input id="addpassword" name="password" type="text" class="form-control" placeholder="fill password" />
+                                                        </div>
+                                                    </div>
+                                                    <div class="modal-footer border-0">
+                                                        <button type="submit" id="addRowButton" class="btn btn-primary">
+                                                            Add
+                                                        </button>
+                                                        <button type="button" class="btn btn-danger" data-dismiss="modal">
+                                                            Close
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </form>
                                         </div>
+
                                     </div>
                                 </div>
+                            </div>
 
-                                <div class="table-responsive">
-                                    <table id="add-row" class="display table table-striped table-hover">
-                                        <thead>
-                                            <tr>
-                                                <th>Full Name</th>
-                                                <th>Email</th>
-                                                <th>Phone</th>
-                                                <th>Position</th>
-                                                <th style="width: 10%">Action</th>
-                                            </tr>
-                                        </thead>
-                                        
-                                        <tbody>
-                                            <?php foreach($staffs as $staff) : ?>
+                            <div class="table-responsive">
+                                <table id="add-row" class="display table table-striped table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>Full Name</th>
+                                            <th>Email</th>
+                                            <th>Phone</th>
+                                            <th>Position</th>
+                                            <th style="width: 10%">Action</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <?php foreach ($staffs as $staff) : ?>
                                             <tr>
                                                 <td><?php echo $staff['FullName'] ?></td>
                                                 <td><?php echo $staff['Email'] ?></td>
@@ -125,16 +137,16 @@ include './traitement/staff.php'
                                                     </div>
                                                 </td>
                                             </tr>
-                                            <?php endforeach; ?>
-                                        </tbody>
-                                    </table>
-                                </div>
+                                        <?php endforeach; ?>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
     <?php include './links/script.php' ?>
 </body>
--- admin/traitement/staff.php
+++ admin/traitement/staff.php
@@ -1,5 +1,14 @@
 <?php 
-include '../config/db.php';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Heroku";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 $staffs = [];
 $sql = "SELECT * FROM staff";
@@ -9,5 +18,25 @@ if ($result->num_rows > 0) {
         $staffs[] = $row;
     }
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $password = $_POST['password'];
+    $role = $_POST['position'];
+
+    $sqlstaff = 'INSERT INTO staff (FullName, Email, Password, Phone, Position) Values (?, ?, ?, ?, ?)';
+    $stmt = $conn->prepare($sqlstaff);
+    $stmt->bind_param('sssss', $name, $email, $password, $phone, $role);
+    $stmt->execute();
+    header('Location: ../staff.php');
+
+    
+
+}
+
+
 
 ?>
