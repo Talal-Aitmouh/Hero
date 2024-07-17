834750d794b9cd76576cd136f9ec0163d49a597a
Talal Ait-Mouh <148981557+Talal-Aitmouh@users.noreply.github.com>
2024-07-17 17:12:36 +0100

    MINOR changes

--- admin/guests.php
+++ admin/guests.php
@@ -8,108 +8,121 @@ include './traitement/guests.php'
 
 <body>
     <div class="wrapper">
-    <?php
+        <?php
         $current_page = 'guests';
         include './links/sidebare.php'; ?>
     </div>
-        <!-- end sidebar -->
-        <div class="main-panel">
-            <?php include './links/customer.php' ?>
-            <div class="container">
-                <div class="page-inner">
-                    <div class="page-header">
-                        <h3 class="fw-bold mb-3">Guests</h3>
-                        <ul class="breadcrumbs mb-3">
-                            <li class="nav-home">
-                                <a href="#">
-                                    <i class="fas fa-user"></i>
-                                </a>
-                            </li>
+    <!-- end sidebar -->
+    <div class="main-panel">
+        <?php include './links/customer.php' ?>
+        <div class="container">
+            <div class="page-inner">
+                <div class="page-header">
+                    <h3 class="fw-bold mb-3">Guests</h3>
+                    <ul class="breadcrumbs mb-3">
+                        <li class="nav-home">
+                            <a href="#">
+                                <i class="fas fa-user"></i>
+                            </a>
+                        </li>
 
-                        </ul>
-                    </div>
+                    </ul>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <div class="d-flex align-items-center">
-                                    <h4 class="card-title">Add a Customer</h4>
-                                    <button class="btn btn-primary btn-round ms-auto" data-bs-toggle="modal" data-bs-target="#addRowModal">
-                                        <i class="fa fa-plus"></i>
-                                        Add a Customer
-                                    </button>
-                                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="d-flex align-items-center">
+                                <h4 class="card-title">Add a Customer</h4>
+                                <button class="btn btn-primary btn-round ms-auto" data-bs-toggle="modal" data-bs-target="#addRowModal">
+                                    <i class="fa fa-plus"></i>
+                                    Add a Customer
+                                </button>
                             </div>
-                            <div class="card-body">
-                                <!-- Modal -->
-                                <div class="modal fade" id="addRowModal" tabindex="-1" role="dialog" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header border-0">
-                                                <h5 class="modal-title">
-                                                    <span class="fw-mediumbold"> New</span>
-                                                    <span class="fw-light"> Row </span>
-                                                </h5>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <p class="small">
-                                                    Create a new row using this form, make sure you
-                                                    fill them all
-                                                </p>
-                                                <form>
-                                                    <div class="row">
-                                                        <div class="col-sm-12">
-                                                            <div class="form-group form-group-default">
-                                                                <label>Name</label>
-                                                                <input id="addName" type="text" class="form-control" placeholder="fill name" />
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-md-6 pe-0">
-                                                            <div class="form-group form-group-default">
-                                                                <label>Position</label>
-                                                                <input id="addPosition" type="text" class="form-control" placeholder="fill position" />
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-md-6">
-                                                            <div class="form-group form-group-default">
-                                                                <label>Office</label>
-                                                                <input id="addOffice" type="text" class="form-control" placeholder="fill office" />
-                                                            </div>
+                        </div>
+                        <div class="card-body">
+                            <!-- Modal -->
+                            <div class="modal fade" id="addRowModal" tabindex="-1" role="dialog" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header border-0">
+                                            <h5 class="modal-title">
+                                                <span class="fw-mediumbold"> New</span>
+                                                <span class="fw-light"> Guest </span>
+                                            </h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+
+                                            <form action="./traitement/guests.php" method="post">
+                                                <div class="row">
+                                                    <div class="col-md-4">
+                                                        <div class="form-group form-group-default">
+                                                            <label>Name</label>
+                                                            <input name="name" id="addName" type="text" class="form-control" placeholder="Enter name" />
                                                         </div>
                                                     </div>
-                                                </form>
-                                            </div>
-                                            <div class="modal-footer border-0">
-                                                <button type="button" id="addRowButton" class="btn btn-primary">
-                                                    Add
-                                                </button>
-                                                <button type="button" class="btn btn-danger" data-dismiss="modal">
-                                                    Close
-                                                </button>
-                                            </div>
+                                                    <div class="col-md-4">
+                                                        <div class="form-group form-group-default">
+                                                            <label>Phone</label>
+                                                            <input name="phone" id="addPhone" type="text" class="form-control" placeholder="Enter phone number" />
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                        <div class="form-group form-group-default">
+                                                            <label>Nationality</label>
+                                                            <input name="nationality" id="addNationality" type="text" class="form-control" placeholder="Enter nationality" />
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-12">
+                                                        <div class="form-group form-group-default">
+                                                            <label>Email</label>
+                                                            <input name="email" id="addEmail" type="email" class="form-control" placeholder="Enter email" />
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-sm-12">
+                                                        <div class="form-group form-group-default">
+                                                            <label>Address</label>
+                                                            <input name="address" id="addAddress2" type="text" class="form-control" placeholder="Enter address" />
+                                                        </div>
+                                                    </div>
+
+
+                                                    <div class="modal-footer border-0">
+                                                        <button type="submit" id="addRowButton" name="add" class="btn btn-primary">
+                                                            Add
+                                                        </button>
+                                                        <button type="button" class="btn btn-danger" data-dismiss="modal">
+                                                            Close
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </form>
                                         </div>
+
                                     </div>
                                 </div>
+                            </div>
 
-                                <div class="table-responsive">
-                                    <table id="add-row" class="display table table-striped table-hover">
-                                        <thead>
-                                            <tr>
-                                                <th>Full Name</th>
-                                                <th>Email</th>
-                                                <th>Phone</th>
-                                                <th>CheckInDate</th>
-                                                <th>CheckOutDate</th>
-                                                <th style="width: 10%">Action</th>
-                                            </tr>
-                                        </thead>
-                                        
-                                        <tbody>
-                                            <?php foreach($guests as $guest) : ?>
+                            <div class="table-responsive">
+                                <table id="add-row" class="display table table-striped table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>Full Name</th>
+                                            <th>Email</th>
+                                            <th>Phone</th>
+                                            <th>CheckInDate</th>
+                                            <th>CheckOutDate</th>
+                                            <th style="width: 10%">Action</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <?php foreach ($guests as $guest) : ?>
                                             <tr>
                                                 <td><?php echo $guest['Name'] ?></td>
                                                 <td><?php echo $guest['Email'] ?> </td>
@@ -130,16 +143,16 @@ include './traitement/guests.php'
                                                     </div>
                                                 </td>
                                             </tr>
-                                            <?php endforeach; ?>
-                                        </tbody>
-                                    </table>
-                                </div>
+                                        <?php endforeach; ?>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
     <?php include './links/script.php' ?>
 </body>
--- admin/traitement/guests.php
+++ admin/traitement/guests.php
@@ -1,5 +1,17 @@
 <?php
-include '../config/db.php';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Heroku";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 
 $guests = [];
 
@@ -13,9 +25,43 @@ if ($result->num_rows > 0) {
 }
 
 
-  
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add'])){
+    
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $nationality = $_POST['nationality'];
+    $address = $_POST['address'];
+
+    $sqlguest = 'INSERT INTO guests (Name, Email, Nationality, Phone, Address) Values (?, ?, ?, ?, ?)';
+    $stmt = $conn->prepare($sqlguest);
+    $stmt->bind_param('sssss', $name, $email, $nationality, $phone, $address);
+    $stmt->execute();
+    header('Location: ../guests.php');
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['update']) ){
+    $name1 = $_POST['name'];
+    $email1 = $_POST['email'];
+    $phone1 = $_POST['phone'];
+    $nationality1 = $_POST['nationality'];
+    $address1 = $_POST['address'];
+    $id = $_POST['id'];
+
+    $sqlguest1 = "UPDATE guests SET Name = ?, Email = ?, Nationality = ?, Phone = ?, Address = ? WHERE GuestID = ?";
+    $stmt = $conn->prepare($sqlguest1);
+    $stmt->bind_param('sssssi', $name1, $email1, $nationality1, $phone1, $address1, $id);
+
+    if ($stmt->execute()){
+        header('Location: ../guests.php');
+        exit();
+    }else{
+        echo "Error updating record: " . $conn->error;
+    }
+    $stmt->close();
 
 
+}
 
 
 
