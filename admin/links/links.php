d2e41f9abb08331dad5ba79955be345251ab27ad
Talal Ait-Mouh <148981557+Talal-Aitmouh@users.noreply.github.com>
2024-07-26 21:24:13 +0100

    minores changes

--- admin/links/links.php
+++ admin/links/links.php
@@ -38,4 +38,7 @@
 
     <!-- CSS Just for demo purpose, don't include it in your project -->
     <link rel="stylesheet" href="assets/css/demo.css" />
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   </head>
