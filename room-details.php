0a7401335721052e6de1066963c709386ad0e317
Talal Ait-Mouh <148981557+Talal-Aitmouh@users.noreply.github.com>
2024-08-13 20:24:33 +0100

    minore changes

--- room-details.php
+++ room-details.php
@@ -1,3 +1,34 @@
+<?php
+// Database connection (replace with your actual connection details)
+include './config/db.php';
+
+// Get the room ID from the URL
+$room_id = isset($_GET['room_id']) ? (int)$_GET['room_id'] : 0;
+
+if ($room_id > 0) {
+    // Fetch room details
+    $query = "SELECT * FROM rooms WHERE RoomID = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $room_id);
+    $stmt->execute();
+    $room_result = $stmt->get_result();
+    $room = $room_result->fetch_assoc();
+    
+    // Fetch room images
+    $query_images = "SELECT ImagePath FROM roomimages WHERE RoomID = ?";
+    $stmt_images = $conn->prepare($query_images);
+    $stmt_images->bind_param("i", $room_id);
+    $stmt_images->execute();
+    $images_result = $stmt_images->get_result();
+    $images = $images_result->fetch_all(MYSQLI_ASSOC);
+} else {
+    echo "Invalid Room ID.";
+    exit;
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="zxx">
 
@@ -111,142 +142,121 @@
     <!-- Header Section End -->
 
     <!-- Room Details Slider Begin -->
-    <div class="room-details-slider">
-        <div class="container">
-            <div class="room__details__pic__slider owl-carousel">
-                <div class="room__details__pic__slider__item set-bg" data-setbg="img/rooms/details/rd-1.jpg"></div>
-                <div class="room__details__pic__slider__item set-bg" data-setbg="img/rooms/details/rd-2.jpg"></div>
-                <div class="room__details__pic__slider__item set-bg" data-setbg="img/rooms/details/rd-3.jpg"></div>
-                <div class="room__details__pic__slider__item set-bg" data-setbg="img/rooms/details/rd-4.jpg"></div>
-            </div>
+    <!-- Room Details Slider Begin -->
+<div class="room-details-slider">
+    <div class="container">
+        <div class="room__details__pic__slider owl-carousel">
+        <div class="room__details__pic__slider__item set-bg" data-setbg="img/rooms/<?php echo $room['Image']; ?>"></div>
+
+            <?php foreach ($images as $image): ?>
+                <div class="room__details__pic__slider__item set-bg" data-setbg="img/rooms/<?php echo $image['ImagePath']; ?>"></div>
+            <?php endforeach; ?>
         </div>
     </div>
-    <!-- Room Details Slider End -->
+</div>
+<!-- Room Details Slider End -->
 
-    <!-- Rooms Details Section Begin -->
-    <section class="room-details spad">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="room__details__content">
-                        <div class="room__details__rating">
-                            <div class="room__details__hotel">
-                                <span>Hotel</span>
-                                <div class="room__details__hotel__rating">
-                                    <span class="icon_star"></span>
-                                    <span class="icon_star"></span>
-                                    <span class="icon_star"></span>
-                                    <span class="icon_star"></span>
-                                    <span class="icon_star-half_alt"></span>
-                                </div>
-                            </div>
-                            <div class="room__details__advisor">
-                                <img src="img/rooms/details/tripadvisor.png" alt="">
-                                <div class="room__details__advisor__rating">
-                                    <span class="icon_star"></span>
-                                    <span class="icon_star"></span>
-                                    <span class="icon_star"></span>
-                                    <span class="icon_star"></span>
-                                    <span class="icon_star-half_alt"></span>
-                                </div>
-                                <span class="review">(1000 Reviews)</span>
+<!-- Rooms Details Section Begin -->
+<section class="room-details spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="room__details__content">
+                    <div class="room__details__rating">
+                        <div class="room__details__hotel">
+                            <span>Hotel</span>
+                            <div class="room__details__hotel__rating">
+                                <span class="icon_star"></span>
+                                <span class="icon_star"></span>
+                                <span class="icon_star"></span>
+                                <span class="icon_star"></span>
+                                <span class="icon_star-half_alt"></span>
                             </div>
                         </div>
-                        <div class="room__details__title">
-                            <h2>Premium King Room</h2>
-                            <a href="#" class="primary-btn">Booking Now</a>
+                        <div class="room__details__advisor">
+                            <img src="img/rooms/details/tripadvisor.png" alt="">
+                            <div class="room__details__advisor__rating">
+                                <span class="icon_star"></span>
+                                <span class="icon_star"></span>
+                                <span class="icon_star"></span>
+                                <span class="icon_star"></span>
+                                <span class="icon_star-half_alt"></span>
+                            </div>
+                            <span class="review">(1000 Reviews)</span>
                         </div>
-                        <div class="room__details__desc">
-                            <h2>Description:</h2>
-                            <p>We’re halfway through the summer, but while plenty of people are kicking back and
-                                enjoying their vacations, the social media development teams likely aren’t doing the
-                                same. In the past two weeks alone, we’ve seen four big new updates that can directly
-                                impact the social marketing campaigns of hotels, resorts, and other businesses in the
-                                hospitality industry. Let’s take a close look at each one.</p>
-                            <p>The new desktop version of the site is significantly improved, which will make it easier
-                                for hotels and resorts to navigate the platform.</p>
-                            <p>There is one big change though that we want to note, and that’s the more live video and
-                                local moments (the latter of which are based on your location). These will be
-                                prioritized in users’ feeds, so take advantage of this and create this content to
-                                improve your reach and connect with more members of your target audience.</p>
-                            <p>We’ve gotten yet another new feature for Instagram Stories, and this time it’s the Chat
-                                sticker, which allows you to invite Story followers to join in on a new group chat.
-                                Instagram is currently advertising this as a way to jumpstart big group conversations or
-                                make plans.</p>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-6 col-md-6 col-sm-6">
-                                <div class="room__details__facilities">
-                                    <h2>Others facilities:</h2>
-                                    <div class="row">
-                                        <div class="col-lg-6">
-                                            <ul>
-                                                <li><span class="icon_check"></span> Takami Bridal Attire</li>
-                                                <li><span class="icon_check"></span> Esthetic Salon</li>
-                                                <li><span class="icon_check"></span> Multilingual staff</li>
-                                                <li><span class="icon_check"></span> Dry cleaning and laundry</li>
-                                                <li><span class="icon_check"></span> Credit cards accepted</li>
-                                            </ul>
-                                        </div>
-                                        <div class="col-lg-6">
-                                            <ul>
-                                                <li><span class="icon_check"></span> Rent-a-car</li>
-                                                <li><span class="icon_check"></span> Reservation & confirmation</li>
-                                                <li><span class="icon_check"></span> Babysitter upon request</li>
-                                                <li><span class="icon_check"></span> 24-hour currency exchange</li>
-                                                <li><span class="icon_check"></span> 24-hour Manager on Duty</li>
-                                            </ul>
-                                        </div>
+                    </div>
+                    <div class="room__details__title">
+                        <h2><?php echo $room['RoomName']; ?></h2>
+                        <a href="#" class="primary-btn">Booking Now</a>
+                    </div>
+                    <div class="room__details__desc">
+                        <h2>Description:</h2>
+                        <p><?php echo $room['Description']; ?></p>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <div class="room__details__facilities">
+                                <h2>Others facilities:</h2>
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <ul>
+                                            <li><span class="icon_check"></span> Takami Bridal Attire</li>
+                                            <li><span class="icon_check"></span> Esthetic Salon</li>
+                                            <li><span class="icon_check"></span> Multilingual staff</li>
+                                            <li><span class="icon_check"></span> Dry cleaning and laundry</li>
+                                            <li><span class="icon_check"></span> Credit cards accepted</li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <ul>
+                                            <li><span class="icon_check"></span> Rent-a-car</li>
+                                            <li><span class="icon_check"></span> Reservation & confirmation</li>
+                                            <li><span class="icon_check"></span> Babysitter upon request</li>
+                                            <li><span class="icon_check"></span> 24-hour currency exchange</li>
+                                            <li><span class="icon_check"></span> 24-hour Manager on Duty</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-6 col-md-6 col-sm-6">
-                                <div class="room__details__more__facilities">
-                                    <h2>Most popular facilities:</h2>
-                                    <div class="row">
-                                        <div class="col-lg-6">
-                                            <div class="room__details__more__facilities__item">
-                                                <div class="icon"><img src="img/rooms/details/facilities/fac-1.png"
-                                                        alt=""></div>
-                                                <h6>Air Conditioning</h6>
-                                            </div>
-                                            <div class="room__details__more__facilities__item">
-                                                <div class="icon"><img src="img/rooms/details/facilities/fac-2.png"
-                                                        alt=""></div>
-                                                <h6>Cable TV</h6>
-                                            </div>
-                                            <div class="room__details__more__facilities__item">
-                                                <div class="icon"><img src="img/rooms/details/facilities/fac-3.png"
-                                                        alt=""></div>
-                                                <h6>Free drinks</h6>
-                                            </div>
-                                            <div class="room__details__more__facilities__item">
-                                                <div class="icon"><img src="img/rooms/details/facilities/fac-4.png"
-                                                        alt=""></div>
-                                                <h6>Unlimited Wifi</h6>
-                                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <div class="room__details__more__facilities">
+                                <h2>Most popular facilities:</h2>
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="room__details__more__facilities__item">
+                                            <div class="icon"><img src="img/rooms/details/facilities/fac-1.png" alt=""></div>
+                                            <h6>Air Conditioning</h6>
                                         </div>
-                                        <div class="col-lg-6">
-                                            <div class="room__details__more__facilities__item">
-                                                <div class="icon"><img src="img/rooms/details/facilities/fac-5.png"
-                                                        alt=""></div>
-                                                <h6>Restaurant quality</h6>
-                                            </div>
-                                            <div class="room__details__more__facilities__item">
-                                                <div class="icon"><img src="img/rooms/details/facilities/fac-6.png"
-                                                        alt=""></div>
-                                                <h6>Service 24/24</h6>
-                                            </div>
-                                            <div class="room__details__more__facilities__item">
-                                                <div class="icon"><img src="img/rooms/details/facilities/fac-7.png"
-                                                        alt=""></div>
-                                                <h6>Gym Centre</h6>
-                                            </div>
-                                            <div class="room__details__more__facilities__item">
-                                                <div class="icon"><img src="img/rooms/details/facilities/fac-8.png"
-                                                        alt=""></div>
-                                                <h6>Spa & Wellness</h6>
-                                            </div>
+                                        <div class="room__details__more__facilities__item">
+                                            <div class="icon"><img src="img/rooms/details/facilities/fac-2.png" alt=""></div>
+                                            <h6>Cable TV</h6>
+                                        </div>
+                                        <div class="room__details__more__facilities__item">
+                                            <div class="icon"><img src="img/rooms/details/facilities/fac-3.png" alt=""></div>
+                                            <h6>Free drinks</h6>
+                                        </div>
+                                        <div class="room__details__more__facilities__item">
+                                            <div class="icon"><img src="img/rooms/details/facilities/fac-4.png" alt=""></div>
+                                            <h6>Unlimited Wifi</h6>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="room__details__more__facilities__item">
+                                            <div class="icon"><img src="img/rooms/details/facilities/fac-5.png" alt=""></div>
+                                            <h6>Restaurant quality</h6>
+                                        </div>
+                                        <div class="room__details__more__facilities__item">
+                                            <div class="icon"><img src="img/rooms/details/facilities/fac-6.png" alt=""></div>
+                                            <h6>Service 24/24</h6>
+                                        </div>
+                                        <div class="room__details__more__facilities__item">
+                                            <div class="icon"><img src="img/rooms/details/facilities/fac-7.png" alt=""></div>
+                                            <h6>Gym Centre</h6>
+                                        </div>
+                                        <div class="room__details__more__facilities__item">
+                                            <div class="icon"><img src="img/rooms/details/facilities/fac-8.png" alt=""></div>
+                                            <h6>Spa & Wellness</h6>
                                         </div>
                                     </div>
                                 </div>
@@ -257,6 +267,8 @@
             </div>
         </div>
     </section>
+    <!-- Rooms Details Section End -->
+
     <!-- Rooms Details Section End -->
 
     <!-- Footer Section Begin -->
