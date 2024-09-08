4d255fd1c95a7b67b0a8970cecc1254a12640bf4
Talal Ait-Mouh <148981557+Talal-Aitmouh@users.noreply.github.com>
2024-09-09 00:25:39 +0100

    adding some plugin.css

--- admin/links/links.php
+++ admin/links/links.php
@@ -31,6 +31,7 @@
       });
     </script>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/plugins.min.css" />
     <link rel="stylesheet" href="assets/css/kaiadmin.min.css" />
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
